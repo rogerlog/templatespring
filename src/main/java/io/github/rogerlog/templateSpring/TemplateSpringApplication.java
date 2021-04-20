d59742beb622b3fe99161467921b3af0f47b2572
Roger <logroger@gmail.com>
2021-04-19 22:20:08 -0300

    first

---
+++ src/main/java/io/github/rogerlog/templateSpring/TemplateSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog.templateSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rogerlog/templateSpring/TemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog.templateSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
